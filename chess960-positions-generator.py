a29c03a6562e9e52a159d1d065e7a734f99ddf16
Kipjonto <124668121+Kipjonto@users.noreply.github.com>
2023-08-26 13:51:20 +0300

    Add files via upload

---
+++ chess960-positions-generator.py
@@ -0,0 +1,25 @@
+from random import randint
+
+def generate_position():
+    position = ['♖', '♔', '♖']
+    non_placed_pieces = ['♗', '♘', '♘', '♕']
+
+    for i in range(4):
+        piece = non_placed_pieces.pop()
+        index = randint(0, len(position)-1)
+        position.insert(index, piece)
+
+    bishopIndex = position.index('♗')
+    secondBishopIndex = randint(bishopIndex + 1, len(position))
+
+    if (bishopIndex % 2 == secondBishopIndex % 2):
+        if (secondBishopIndex == 7):
+            secondBishopIndex -= 1
+        else:
+            secondBishopIndex += 1
+
+    position.insert(secondBishopIndex, '♗')
+
+    return position
+
+print(generate_position())
