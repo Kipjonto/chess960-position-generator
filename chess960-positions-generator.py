af1c766a56b5887b8fce06eb8b8570dc99b909d9
Kirill <124668121+Kipjonto@users.noreply.github.com>
2023-12-12 09:27:39 +0500

    Update chess960-positions-generator.py

--- chess960-positions-generator.py
+++ chess960-positions-generator.py
@@ -2,13 +2,12 @@ from random import randint
 
 def generate_position():
     position = ['♖', '♔', '♖']
-    non_placed_pieces = ['♗', '♘', '♘', '♕']
-
+    unplaced_pieces = ['♗', '♘', '♘', '♕']
     for i in range(4):
-        piece = non_placed_pieces.pop()
+        piece = unplaced_pieces.pop()
         index = randint(0, len(position)-1)
         position.insert(index, piece)
-
+    // Bishops must be on even and odd indexes
     bishopIndex = position.index('♗')
     secondBishopIndex = randint(bishopIndex + 1, len(position))
     if (bishopIndex % 2 == secondBishopIndex % 2):
